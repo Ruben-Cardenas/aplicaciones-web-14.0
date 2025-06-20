c6903e38b07f4f0866d4c9dd4c341045e439b0e7
Ruben-Cardenas <cardenasruben2004@gmail.com>
2025-06-20 08:40:08 -0600

    commit 20/06/2025

--- appweb/src/components/Dashboard.tsx
+++ appweb/src/components/Dashboard.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Layout } from 'antd';
+import { Header, Content, Footer } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import MenuDynamic from './MenuDynamic';
 
-export default function Dashboard() {
+function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider width={220}>
+        <MenuDynamic />
+      </Sider>
+
+      <Layout>
+        <Header />
+        <Content style={{ margin: '24px 16px 0', padding: 24, background: '#fff' }}>
+          <Outlet />
+        </Content>
+        <Footer />
+      </Layout>
+    </Layout>
+  );
 }
+
+export default Dashboard;
+
